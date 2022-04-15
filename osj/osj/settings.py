4a6cbc826807b8f6c611290533cf6a13116e3085
Adam Zeloof <azzeloof@gmail.com>
2022-04-15 16:47:07 +0000

    alpha deployment prep

--- osj/osj/settings.py
+++ osj/osj/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
