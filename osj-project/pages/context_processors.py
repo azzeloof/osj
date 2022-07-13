980f010f6da87ae05f9119e13f974403e8e94be2
Adam Zeloof <azzeloof@gmail.com>
2022-07-13 11:34:09 +0100

    Footer updates

--- osj-project/pages/context_processors.py
+++ osj-project/pages/context_processors.py
@@ -2,3 +2,9 @@ from osj import settings
 
 def google_analytics_key(request):
     return {'google_analytics_key': settings.GOOGLE_ANALYTICS_KEY}
+
+def kofi_creds(request):
+    return {
+        'kofi_username': settings.KOFI_USERNAME,
+        'kofi_key': settings.KOFI_KEY
+        }
